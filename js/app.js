7368652959eac669e0496a4cf3074c5ba8b21d65
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-23 16:11:54 +0100

    Added form submit event listener

--- js/app.js
+++ js/app.js
@@ -12,4 +12,9 @@ window.onload = function() {
 
 		return result.join("");
 	}
+
+	let form = document.getElementById("form");
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+	});
 };
