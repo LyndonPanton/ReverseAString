c13668a43ab66bf7ba7f1eddd0337fd5df66d628
Lyndon Panton <LyndonPanton@gmail.com>
2019-06-23 16:10:13 +0100

    Defined function for reversing string

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@
 
 window.onload = function() {
 	document.getElementById("copyright-year").textContent = (new Date()).getFullYear();
+
+	function reverseString(str) {
+		let result = [];
+
+		for (let i = 0; i < str.length; i++) {
+			result.unshift(str[i]);
+		}
+
+		return result.join("");
+	}
 };
